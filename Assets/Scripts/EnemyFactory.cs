336585f60e8c701aba834fb50903a72faa9e8dc2
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-09-05 11:13:40 +1000

    PC_Work commit

--- Assets/Scripts/EnemyFactory.cs
+++ Assets/Scripts/EnemyFactory.cs
@@ -11,7 +11,6 @@ public class EnemyFactory : MonoBehaviour
         new Vector3(0, 0, 5 ),
         new Vector3(0, 0, -5 ),
         new Vector3(5, 0, 0) };
-    private GameObject _enemy;
 
     private void Start()
     {
@@ -21,21 +20,17 @@ public class EnemyFactory : MonoBehaviour
     private IEnumerator SpawnEnemies()
     {
         var waitForTwoSeconds = new WaitForSeconds(2f);
+        int enemyLastStartPosition = _points.Length - 1;
 
-        if (_enemy == null)
+        for (int i = 0; i < _points.Length; i++)
         {
-            int enemyLastStartPosition = _points.Length - 1;
+            var _enemy = Instantiate(_enemyPrefab, _points[i], Quaternion.Euler(0f,0f,0f));
+            _enemy.transform.position = _points[i];
 
-            for (int i = 0; i < _points.Length; i++)
-            {
-                _enemy = Instantiate(_enemyPrefab);
-                _enemy.transform.position = _points[i];
+            if (i == enemyLastStartPosition)
+                i = 0;
 
-                if (i == enemyLastStartPosition)
-                    i = 0;
-
-                yield return waitForTwoSeconds;
-            }
+            yield return waitForTwoSeconds;
         }
     }
 }
